5786b2cee0d671d485142f0000e932cb76a921d3
KajE879 <9024897@student.zadkine.nl>
2026-03-12 09:31:52 +0100

    Game Basic Explanation
    Made Cards and Suits with Value, Decks and Player Hand

---
+++ test/PlayerHand.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test
+{
+    internal class PlayerHand
+    {
+        List<Card> Hand;
+        //int MaxCards;
+        List<int> SelectedIndexes;
+
+        public PlayerHand(int maxCards)
+        {
+            this.MaxCards = maxCards;
+            this.Hand = new List<Card>();
+            this.SelectedIndexes = new List<int>();
+        }
+
+        public int MaxCards { get; private set; }
+
+        public void AddCard(Card newCard)
+        {
+            this.Hand.Add(newCard);
+        }
+
+        public void SelectCard(int index)
+        {
+            if(this.Hand.Count == 5)
+            if (this.Hand.Count > index)
+            {
+                if (!this.SelectedIndexes.Contains(index))
+                {
+                    this.SelectedIndexes.Add(index);
+                }
+            }
+        }
+
+        public void DeselectCard(int index)
+        {
+            SelectedIndexes.Remove(index);
+        }
+
+        public List<Card> GetSelected()
+        {
+            return this.Hand.
+               Where((Card, index) =>
+                {
+                    if (this.SelectedIndexes.Contains(index))
+                    {
+                        return true;
+                    }
+                    return false;
+                })
+            .ToList();
+        }
+
+        public void RemoveSelected()
+        {
+            this.Hand = this.Hand.
+               Where((Card, index) =>
+               {
+                   if (this.SelectedIndexes.Contains(index))
+                   {
+                       return false;
+                   }
+                   return true;
+               })
+            .ToList();
+            this.SelectedIndexes.Clear();
+        }
+    }
+}
---
+++ test/Suit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test
+{
+    enum Suit
+    {
+        Hearts,
+        Diamonds,
+        Spades,
+        Clovers
+    }
+}
